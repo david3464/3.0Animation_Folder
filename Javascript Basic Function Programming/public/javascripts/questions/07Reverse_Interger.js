2178f135da7b97b7df375f4a60c57a335823d5b5
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-18 12:11:40 -0400

    1

--- Javascript Basic Function Programming/public/javascripts/questions/07Reverse_Interger.js
+++ Javascript Basic Function Programming/public/javascripts/questions/07Reverse_Interger.js
@@ -4,10 +4,41 @@ Specifications
     Output 123
 */
 // solution 1
-var reverse = function (x) {
+var RI07_1 = function(num) {
+    let y;
+    if(num < 0){
+        y=num.toString().split('').reverse();
+        console.log(y);
+        y.unshift('-');
+        y.pop();
+        y = parseInt(y.join(''));
+        //Check for 32 bit outside of bounds
+        if(y < -2147483648 ) {
+            return 0
+        }
+        //positive case start here
+    } else {
+        y = parseInt(num.toString().split('').reverse().join(''));
+        //Check for 32 bit outside of bounds
+        if (y > 2147483648) {
+            return 0
+        } 
+    }
+    return y
+}
+
+// solution 2 
+var RI07_2 = function (num) {
+    var a = num.toString().split('').reverse().join('');
+    //split is return the first one ps: if negative sign is return NaN
+    return parseInt(a)*Math.sign(num);
+}
+
+// solution 3
+var RI07_3 = function (num) {
     //create xstring var to change x to string
     //create resutlt to return at the end (empty string)
-    var xstring = x.toString();
+    var xstring = num.toString();
     var result = '';
     //if (xstring[first])is '-' 
         //append '-' to result string
@@ -26,9 +57,9 @@ var reverse = function (x) {
     }
     //xstring that is perfectly ready to go through a for loop
     //reverse for loop throught x string
-    return result;
+    return (parseInt(result));
 };
 
-console.log(reverse(123));
-console.log(reverse(-456));
-console.log(reverse(560));
+console.log(RI07_3(1230));
+console.log(RI07_3(-4560));
+console.log(RI07_3(5600));
