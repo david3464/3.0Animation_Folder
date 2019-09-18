2a1ecfb747b891d3d139e90e544a60434c92bd8e
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-18 12:16:07 -0400

    1

--- Javascript Basic Function Programming/public/javascripts/lectures/04.js
+++ Javascript Basic Function Programming/public/javascripts/lectures/04.js
@@ -1,42 +1,14 @@
 window.onload = function() {
-    var btn1 = document.getElementById('btn1');
-    btn1.onclick = function() {
-        alert('1')
-    }
-
-    // var oDivs = document.getElementsByTagName ('div');
-    // // oDiv[1].style.background ='red';
-    // console.log(oDivs.length);
-    // for(let i=0; i<oDivs.length; i++) {
-    //     oDivs[i].style.background = 'red';
-    // }
-    // for(var i = 0; i <)
-
-    var btn2 = document.getElementById('btn2'); 
-    var btn3 = document.getElementById('btn3');
-    var btn4 = document.getElementById('btn4');
-    var oDiv6 =document.getElementById('div6');
-    var aCh = oDiv6.getElementsByTagName ('input');
     
-    btn2.onclick = function () {
-        for(var i=0; i<aCh.length; i++){
-            aCh[i].checked = true;
-        }
-    };
-    btn3.onclick = function () {
-        for(var i=0; i<aCh.length; i++){
-            aCh[i].checked = false;
-        }
-    };
-    btn4.onclick = function () {
-        for(var i=0; i<aCh.length; i++){
-            if(aCh[i].checked==true) {
-                aCh[i].checked = false;
-            }
-            else {
-                aCh[i].checked = true;
-            }
-        }
-    };
+var btn1 = document.getElementById('btn1');
+btn1.onclick = function() {
+    alert('123')}
+
+
+var aDiv = document.getElementsByTagName ('div');
+
+aDiv[1].style.background ='red'
+
+
 }
 
--- Javascript Basic Function Programming/routes/preps.js
+++ Javascript Basic Function Programming/routes/preps.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-// app.use('/func_preps', prepsRouter);
-
-//router address localhost:2001/func_preps/test
-//descriptions: Lecutre One Javascript
+//router address localhost:2001/func_preps/01
+//descriptions: Preprequist Course One Javascript
 //comments: 
-router.get('/test', function(req, res, next) {
-    res.render('functions/preps/test');
+router.get('/01', function(req, res, next) {
+    res.render('functions/preps/lecture01');
   });
 
 
