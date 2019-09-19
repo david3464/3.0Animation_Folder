7a474332aa6bf7ba302f2fb258953a9b24cc010d
david3464 <51375293+david3464@users.noreply.github.com>
2019-09-19 18:06:08 -0400

    1

--- Javascript Basic Function Programming/routes/vues.js
+++ Javascript Basic Function Programming/routes/vues.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-//router address localhost:2001/func_tutorials/index
-//descriptions: Tutorial List
+//router address localhost:2001/func_vues/index
+//descriptions: Vue Course One List
 //comments: 
 router.get('/index', function(req, res, next) {
-  res.render('functions/tutorials/index');
+  res.render('functions/vues/index');
 });
 
-//router address localhost:2001/func_tutorials/01
-//descriptions: Tutorials One 
+//router address localhost:2001/func_vues/01
+//descriptions: Vue Course One
 //comments: 
 router.get('/01', function(req, res, next) {
   res.render('functions/tutorials/tut01');
