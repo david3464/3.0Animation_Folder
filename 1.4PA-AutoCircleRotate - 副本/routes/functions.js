731a9d8c054cc9dfb72d83ad44af1a43769629d9
david3464 <51375293+david3464@users.noreply.github.com>
2019-11-07 19:26:50 -0500

    1

--- 1.4PA-AutoCircleRotate - 副本/routes/functions.js
+++ 1.4PA-AutoCircleRotate - 副本/routes/functions.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('functions/1PhotoAnimation/4AutoCircleRotate', { layout: 'hero_layout' });
 });
 
+router.get('/about', function(req, res, next) {
+    res.sendFile('testscript.html', {root: path.join(__dirname, '../files')});
+  });
+  
+
 module.exports = router;
